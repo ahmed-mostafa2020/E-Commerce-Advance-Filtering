f7f4bf11d8c8d540c93376bbaaf558dae197eef2
Ahmed Mostafa <ahmed-mostafa2020>
2023-08-07 22:10:26 +0300

    nav done

---
+++ e-commerce-advance-filtering/src/components/Nav.js
@@ -0,0 +1,33 @@
+import { FiHeart } from "react-icons/fi";
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
+import { Link } from "react-router-dom";
+
+const Nav = ({ handleInputChange, query }) => {
+  return (
+    <nav>
+      <div className="nav-container">
+        <input
+          className="search-input"
+          type="text"
+          onChange={handleInputChange}
+          value={query}
+          placeholder="Enter your search shoes."
+        />
+      </div>
+
+      <div className="profile-container">
+        <Link to={"/favorite"}>
+          <FiHeart className="nav-icons" />
+        </Link>
+        <Link to={"/cart"}>
+          <AiOutlineShoppingCart className="nav-icons" />
+        </Link>
+        <Link to={"/users"}>
+          <AiOutlineUserAdd className="nav-icons" />
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
