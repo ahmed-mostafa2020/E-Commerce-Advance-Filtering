1a7d10eb30043c8ce344051f8d1de11b6612813d
Ahmed Mostafa <ahmed-mostafa2020>
2023-08-08 00:09:33 +0300

    basic done 2

--- e-commerce-advance-filtering/src/components/Recommended.js
+++ e-commerce-advance-filtering/src/components/Recommended.js
@@ -6,11 +6,7 @@ const Recommended = ({ handleClick }) => {
       <div>
         <h2 className="recommended-title">Recommended</h2>
         <div className="recommended-flex">
-          <Button
-            onClickHandler={handleClick}
-            value="all"
-            title="All Products"
-          />
+          <Button onClickHandler={handleClick} value="" title="All Products" />
           <Button onClickHandler={handleClick} value="Nike" title="Nike" />
           <Button onClickHandler={handleClick} value="Adidas" title="Adidas" />
           <Button onClickHandler={handleClick} value="Puma" title="Puma" />
